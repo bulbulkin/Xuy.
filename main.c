6e8ffe48b0f44e2343b15f01bca2c1b1601e66c7
bulbulkin <93879724+bulbulkin@users.noreply.github.com>
2021-11-27 17:54:31 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c,d,f,e;
+scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &f, &e);
+printf("<<%d + %d + %d + %d + %d + %d = %d>>", a, b, c, d, f, e, a + b + c + d + f + e);
+return 0;
+}
+/*
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    printf("«Пора идти отдыхать!»\n");
+    return 0;
+}
+
+#include <stdio.h>
+*/
