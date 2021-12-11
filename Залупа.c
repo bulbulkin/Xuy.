02712068e5f87fe198823db7d8b0d1109dacb16a
bulbulkin <93879724+bulbulkin@users.noreply.github.com>
2021-12-11 17:10:34 +0700

    Add files via upload

---
+++ Залупа.c
@@ -0,0 +1,80 @@
+/*#include <stdio.h>
+int main()
+{
+int a;
+scanf ("%d", &a);
+if (a > 0){printf("+");}
+else if(a == 0){printf("Z");}
+else{printf("-");}
+return 0;
+}
+*/
+
+
+/*#include <stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d %d %d", &a, &b, &c);
+if (a > b && a > c) {if (b > c) {
+        printf("%d, %d, %d\n", a, b, c);}
+else{
+        printf("%d, %d, %d\n", a, c, b);}
+}
+if (b > c && b > a) {if (c > a){
+        printf("%d, %d, %d\n", b, c, a);}
+else{
+        printf("%d, %d, %d\n", b, a, c);}
+}
+if (c > b && c > a) {if (b > a){
+        printf("%d, %d, %d\n", c, b, a);}
+else{
+        printf("%d, %d, %d\n", c, a, b);}
+}
+printf("%d %d %d", a, b, c);
+return 0;
+}
+*/
+
+/*#include <stdio.h>
+int main()
+{
+int a;
+scanf("%d", &a);
+if (a%2!=0){printf("No");}
+else {printf("Yes");}
+return 0;
+}
+*/
+
+/*#include <stdio.h>
+int main()
+{
+int a;
+scanf("%d", &a);
+if (a<=18 && a>=9){printf("Day");}
+else {printf("Night");}
+return 0;
+}
+*/
+
+/*#include <stdio.h>
+int main()
+{
+int a;
+scanf("%d", &a);
+printf("%d", a/100);
+return 0;
+}
+*/
+
+/*#include <stdio.h>
+int main()
+{
+int a, b, c;
+scanf("%d %d %d", &a, &b, &c);
+if (a + b > c || a + c > b || a + c > b){printf("Yes");}
+else {printf("No");}
+return 0;
+}
+*/
